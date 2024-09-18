dc07a829600c52e3eba86dea51eb8cf28c4c5df2
Yann Klein <yann.klein@me.com>
2024-09-18 17:15:50 +0900

    change app components name/data from blog to project repo

---
+++ app/_components/Project.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+
+export type ProjectProps = {
+  id: string;
+  title: string;
+  description: string;
+  owners: string[];
+  status: string;
+  goal: number;
+  bid: number;
+  impacts: string[];
+};
+
+const Project: React.FC<{ project: ProjectProps }> = ({ project }) => {
+  const {id, title, owners, description} = project;
+  return (
+    <Link href={`/projects/${id}`}>
+      <h2>{title}</h2>
+      <small>By {owners.join(", ")}</small>
+      <p>{description}</p>
+    </Link>
+  );
+};
+
+export default Project;
---
+++ utils/getProject.ts
@@ -0,0 +1,15 @@
+
+export const getProject = async () => {
+  const project = {
+    id: "1",
+    title: "Project 1",
+    description: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
+    published: false,
+    owners: ["Sho", "Shoko"],
+    status: "open",
+    goal: 100000,
+    bid: 34000,
+    impacts: ["low carbon", "local"]
+  }
+  return project
+};
