6484e130af4e9daac7582d11a595e3000b9279d9
Adriana Ito <ito.dri@gmail.com>
2024-09-21 23:17:57 +0900

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,16 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     colors: {
-      cream: '#EFEAEB'
+      cream: '#EFEAEB',
+      'coralBlue': '#55A9BB',
+      'white': '#FFFFFF',
+      'lightBlue': '#EDF9FF'
     },
     extend: {
       fontFamily: {
