21fa6f5334648ca85825c5fba83dd4e068060c8c
Adriana Ito <ito.dri@gmail.com>
2024-09-22 00:18:02 +0900

    fix responsiveness

--- app/_components/LandingBanner.tsx
+++ app/_components/LandingBanner.tsx
@@ -8,13 +8,13 @@ const LandingBanner: React.FC = () => {
           alt="Keyboard with World"
           className="w-24 lg:w-48 h-22 lg:h-44 mr-2"
         />
-        <h1 className="text-3xl lg:text-9xl mt-4">CROWD CODED</h1>
+        <h1 className="text-4xl md:text-6xl lg:text-9xl mt-4">CROWD CODED</h1>
       </div>
-      <p className="mt-4 text-xl lg:text-2xl">
+      <p className="mt-4 text-lg md:text-xl lg:text-2xl px-2 lg:px-0">
         People that make the world better are rarely millionaires
       </p>
-      <p className="text-xl lg:text-2xl">But they very often need a <span className="text-coralBlue">website</span></p>
-      <p className="text-xl lg:text-2xl">
+      <p className="text-lg md:text-xl lg:text-2xl px-2 lg:px-0">But they very often need a <span className="text-coralBlue">website</span></p>
+      <p className="text-lg md:text-xl lg:text-2xl px-2 lg:px-0">
         Support them by{' '}
         <span
           className="text-coralBlue"
