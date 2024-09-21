b8cd9333089e89c0a2a0d8da03df9ce49f563a72
Yann Klein <yann.klein@me.com>
2024-09-22 00:04:39 +0900

    fix donation progress bar to make the percentage of progress show up

--- app/_components/DonationProgress.tsx
+++ app/_components/DonationProgress.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Link from "next/link";
 
-const DonationProgress: React.FC<{ progress: Number }> = ({ progress }) => {
+const DonationProgress: React.FC<{ progress: number }> = ({ progress }) => {
   const classComputed = 
-    "before:rounded before:!content-['" + progress + `%'] before:flex before:justify-center before:items-center before:z-1 before:position-absolute before:top-0 before:left-0 before:bg-coralBlue before:text-white before:h-full before:w-[20%]
-    position-relative rounded text-center font-heading bg-white w-full h-8`;
+    "before:rounded before:!content-['" + progress + "%'] before:flex before:justify-center before:items-center before:z-1 before:position-absolute before:top-0 before:left-0 before:bg-coralBlue before:text-white before:h-full before:w-[" + progress + "%] position-relative rounded text-center font-heading bg-white w-full h-8";
   return (
     <>
     <div className={classComputed}></div>
