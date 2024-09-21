694d0807ac2c2779f263c48daf7cc812a2735127
Adriana Ito <ito.dri@gmail.com>
2024-09-21 21:38:01 +0900

    change bg color and add shadow

--- app/_components/Footer/Footer.tsx
+++ app/_components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-50 font-heading">
+    <footer className="bg-[#EFEAEB] font-heading shadow-[0_-35px_60px_-15px_rgba(0,0,0,0.3)] h-28 flex">
       <div className="container mx-auto px-4 py-6 flex justify-between items-center">
         <div className="flex space-x-6">
           <Link href="#" className="text-black font-bold">PROJECTS</Link>
