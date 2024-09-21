984168f28d4d0e7d3280c0fbc5f18aa3f465d8b1
Yann Klein <yann.klein@me.com>
2024-09-21 23:35:35 +0900

    update FeatureProject component and modify its containing layout in the landing page

--- app/_components/FeaturedProject.tsx
+++ app/_components/FeaturedProject.tsx
@@ -1,32 +1,97 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { getImpactIcon } from '@/utils/getImpactIcon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import DonationProgress from './DonationProgress';
 
 export type ProjectProps = {
   id: string;
   title: string;
   description: string;
-  owners: string[];
+  owners: string;
   status: string;
   goal: number;
+  mission: string;
   impacts: string[];
 };
 
 const Project: React.FC<{ project: ProjectProps }> = ({ project }) => {
   const { id, title, owners, description } = project;
+  const impactLogos = project.impacts.map((impact) => getImpactIcon(impact));
+  const progressPct = 20;
   return (
-    <div className="bg-cream drop-shadow-xl p-5">
+    <div className="bg-cream drop-shadow-2xl rounded-md">
       <Link href={`/projects/${id}`}>
-        {/* <Image
-          src="https://sho-farm.sunnyday.jp/wp-content/uploads/2014/10/1415165056949.jpg"
-          alt={title}
-          width={100}
-          height={80}
-        /> */}
-        <img src="https://sho-farm.sunnyday.jp/wp-content/uploads/2014/10/1415165056949.jpg" alt="" />
-        <h2>{title}</h2>
-        <small>By {owners.join(', ')}</small>
-        <p>{description}</p>
+        <div className="sm:flex sm:flex-row-reverse">
+          <div className="flex flex-col justify-between p-8 sm:pb-0">
+            <div className="font-heading">
+              <h2 className="text-4xl">{title}</h2>
+              <small>{owners}</small>
+            </div>
+            <div className="flex gap-2 md:gap-8 py-3">
+              {impactLogos.map((impactLogo) => (
+                <div
+                  className="text-center flex flex-col"
+                  style={{ color: impactLogo.color }}
+                >
+                  <FontAwesomeIcon
+                    className=" lg:!hidden"
+                    icon={impactLogo.icon}
+                    size="2x"
+                  />
+                  <FontAwesomeIcon
+                    className="!hidden lg:!block"
+                    icon={impactLogo.icon}
+                    size="3x"
+                  />
+                  <small>{impactLogo.text}</small>
+                </div>
+              ))}
+            </div>
+            <p className="text-justify">{description}</p>
+          </div>
+          <img
+            className="min-w-full sm:min-w-[260px] lg:min-w-[360px] h-[300px] object-cover object-[50%_20%] sm:rounded-ss-md"
+            src="https://sho-farm.sunnyday.jp/wp-content/uploads/2023/04/%E3%83%9F%E3%83%8B%E3%82%B5%E3%82%A4%E3%82%BA.jpg"
+            alt=""
+          />
+          {/* <Image
+              src="https://sho-farm.sunnyday.jp/wp-content/uploads/2014/10/1415165056949.jpg"
+              alt={title}
+              width={100}
+              height={80}
+            /> */}
+        </div>
+        <div className="p-8">
+          <div className="pb-8">
+            <h3 className="font-heading text-2xl">Crowd Coded Mission</h3>
+            <p className="text-justify">{project.mission}</p>
+          </div>
+          <div className="pb-8">
+            <div className="flex justify-between font-heading">
+              <h3 className="text-2xl">X donators so far</h3>
+              <p className="text-md text-coralBlue">
+                ¥XX OUT OF ¥{project.goal}
+              </p>
+            </div>
+            <DonationProgress progress={progressPct} />
+          </div>
+          <div className="flex justify-end gap-8">
+            <Link
+              className="text-coralBlue bg-white px-1 rounded font-heading border-2 border-coralBlue text-3xl"
+              href={`/`}
+            >
+              Learn more
+            </Link>
+            <Link
+              className="text-white bg-coralBlue px-1 rounded font-heading border-2 border-coralBlue text-3xl"
+              href={`/`}
+            >
+              Donate
+            </Link>
+          </div>
+        </div>
       </Link>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,23 @@
-import React from "react"
-import Project from "@/app/_components/Project"
-import { getProjects } from "@/utils/getProjects";
-
+import React from 'react';
+import { getProjects } from '@/utils/getProjects';
+import Project from '@/app/_components/Project';
+import FeaturedProject from './_components/FeaturedProject';
 
 const Landing = async () => {
-
   const projects = await getProjects();
 
   return (
-      <div className="page">
-        <main>
-          {projects?.map((project) => (
-            <div key={project.id} className="project">
-              <Project project={project} />
-            </div>
-          ))}
-        </main>
+    <div className="page p-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+        <div className="font-heading flex justify-center text-center items-center text-6xl">
+          Today’s project to support
+        </div>
+        <div className="col-span-2">
+          <FeaturedProject project={projects[0]} />
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
