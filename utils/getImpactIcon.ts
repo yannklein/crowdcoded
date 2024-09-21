6b662fcb7e7dbedba0b7416ea9a87e2403e4a825
Yann Klein <yann.klein@me.com>
2024-09-21 23:34:40 +0900

    update getImpact to get icon, color and text for each impact enum

--- utils/getImpactIcon.ts
+++ utils/getImpactIcon.ts
@@ -1,7 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLeaf, faRecycle } from '@fortawesome/free-solid-svg-icons'
+import { faCreativeCommonsZero } from '@fortawesome/free-brands-svg-icons'
+
 export const getImpactIcon = (impact: string) => {
   const icons = {
-    "low carbon": "💥",
-    "local": "🌍",
+    "SUSTAINABLE": {
+      color: "#FFA653",
+      icon: faRecycle,
+      text: "Sustainable",
+    },
+    "ZERO_WASTE": {
+      color: "#8975FF",
+      icon: faCreativeCommonsZero,
+      text: "Zero Waste",},
+    "ORGANIC": {
+      color: "#3AB35C",
+      icon: faLeaf,
+      text: "Organic",
+    },
   }
   return icons[impact]
 } 
