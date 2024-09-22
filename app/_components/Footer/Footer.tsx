3a654a24a3e3f83164c6138ec0163b28419f6b32
Adriana Ito <ito.dri@gmail.com>
2024-09-22 09:56:14 +0900

    fix footer responsiveness

--- app/_components/Footer/Footer.tsx
+++ app/_components/Footer/Footer.tsx
@@ -5,13 +5,13 @@ export const Footer: React.FC = () => {
   return (
     <footer className="bg-cream font-heading drop-shadow-top h-28 flex">
       <div className="container mx-auto px-4 py-6 flex justify-between items-center">
-        <div className="flex space-x-6">
-          <Link href="#" className="text-black font-bold">PROJECTS</Link>
-          <Link href="#" className="text-black font-bold">ABOUT US</Link>
-          <Link href="#" className="text-black font-bold">CONTACT</Link>
+        <div className="flex flex-col lg:flex-row lg:space-x-6">
+          <Link href="#">PROJECTS</Link>
+          <Link href="#">ABOUT US</Link>
+          <Link href="#">CONTACT</Link>
         </div>
-        <div className="text-black font-bold">
-          CREATED WITH <span className="text-red-500">❤️</span> BY ADRIANA AND YANN
+        <div>
+          CREATED WITH <span className="">❤️</span> BY ADRIANA AND YANN
         </div>
       </div>
     </footer>
