19ce16e0d2ff174c2a7ff2ea52754115ed238ae4
Yann Klein <yann.klein@me.com>
2024-09-26 17:11:52 +0900

    modify getProject to pull a certain project from DB according to id

--- utils/getProject.ts
+++ utils/getProject.ts
@@ -1,15 +1,9 @@
+import prisma from '@/lib/prisma';
 
-export const getProject = async () => {
-  const project = {
-    id: "1",
-    title: "Project 1",
-    description: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-    published: false,
-    owners: "Shoko and Sho Nakano",
-    status: "open",
-    goal: 100000,
-    bid: 34000,
-    impacts: ["low carbon", "local"]
-  }
-  return project
+export const getProject = async (id: string) => {
+  return await prisma.project?.findFirst({
+    where: {
+      id: id,
+    },
+  });
 };
