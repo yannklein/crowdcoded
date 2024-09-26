67a0aa720328a2c425e36191a1b983201d4be994
Adriana Ito <ito.dri@gmail.com>
2024-09-26 21:58:45 +0900

    create ImpactfulBusiness component

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,31 @@
-import React from 'react';
-import { getProjects } from '@/utils/getProjects';
-import FeaturedProject from './_components/FeaturedProject';
+import React from "react";
+import { getProjects } from "@/utils/getProjects";
+import FeaturedProject from "./_components/FeaturedProject";
 import LandingBanner from "@components/LandingBanner";
-import HowItWorks from '@components/HowItWorks';
+import HowItWorks from "@components/HowItWorks";
+import ImpactfulBusinesses from "@components/ImpactfulBusiness";
 
 const Landing = async () => {
   const projects = await getProjects();
 
   return (
-      <div className="page">
-        <main>
-          <LandingBanner />
-          <div className="page py-20 px-3 md:px-8">
-            <div className="grid md:grid-cols-3 gap-10">
-              <div className="font-heading flex justify-center text-center items-center text-5xl md:text-6xl">
-                Today’s project to support
-              </div>
-              <div className="md:col-span-2">
-                <FeaturedProject project={projects[0]} />
-              </div>
+    <div className="page">
+      <main>
+        <LandingBanner />
+        <div className="page py-20 px-3 md:px-8">
+          <div className="grid md:grid-cols-3 gap-10">
+            <div className="font-heading flex justify-center text-center items-center text-5xl md:text-6xl">
+              Today’s project to support
+            </div>
+            <div className="md:col-span-2">
+              <FeaturedProject project={projects[0]} />
             </div>
           </div>
-          <HowItWorks />
-        </main>
-      </div>
+        </div>
+        <HowItWorks />
+        <ImpactfulBusinesses />
+      </main>
+    </div>
   );
 };
 
