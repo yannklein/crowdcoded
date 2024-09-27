940367343457cc38d337cc25dbb0e60e3436c99c
Yann Klein <yann.klein@me.com>
2024-09-27 15:44:46 +0900

    create swiper carousel

---
+++ app/_components/ProjectCarousel.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+const ProjectCarousel = ({slides}) => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return `<span style="width: 24px; height: 24px; background-color: #55A9BB" class="${className}">${index + 1}</span>`;
+    },
+  };
+
+  return (
+    <Swiper
+      className="h-[400px] bg-[#000000] text-white rounded-md shadow-md"
+      pagination={pagination}
+      modules={[Pagination]}
+      slidesPerView={1}
+    >
+      {slides.map((slide, index) => (
+        <SwiperSlide>
+          <div className="flex" style={{flexDirection: index % 2 === 0 ? 'row' : 'row-reverse'}}>
+            <p className='w-1/2 p-8 flex items-center' >{slide.text}</p>
+            <img className='w-1/2 h-[400px] object-cover' src={slide.image} alt="" />
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default ProjectCarousel;
