9f55cbb3e5e99b9309bc77d98fd04ecd0933b3bb
Yann Klein <yann.klein@me.com>
2024-09-27 15:45:34 +0900

    completed project detail page with carousel, map, link and details of the work

--- app/(header-layout)/projects/[id]/page.tsx
+++ app/(header-layout)/projects/[id]/page.tsx
@@ -1,20 +1,52 @@
 import React from 'react';
-import { getProject } from '@/utils/getProject';
-import { ProjectProps } from '@/app/types/Props';
-import FundingCard from '@/app/_components/FundingCard';
-import ImpactIcon from '@/app/_components/ImpactIcon';
-import { getImpactIcon } from '@/utils/getImpactIcon';
-import ProjectDetailsMenu from '@/app/_components/ProjectDetailsMenu';
+import Link from 'next/link';
 import Markdown from 'react-markdown';
+
+import { faLaptop } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSquareFacebook,
+  faSquareInstagram,
+  faSquareYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+
+import { getProject } from '@/utils/getProject';
+import { getImpactIcon } from '@/utils/getImpactIcon';
+import { ProjectProps } from '@/app/types/Props';
+
+import ImpactIcon from '@/app/_components/ImpactIcon';
+import FundingCard from '@/app/_components/FundingCard';
+import ProjectDetailsMenu from '@/app/_components/ProjectDetailsMenu';
 import ProjectMilestone from '@/app/_components/ProjectMilestone';
+import ProjectCarousel from '@/app/_components/ProjectCarousel';
 
 const Project = async ({ params }: { params: { id: string } }) => {
   const project: ProjectProps = await getProject(params.id);
   const { title, description, owners, impacts } = project;
   const impactsData = impacts.map((impact: string) => getImpactIcon(impact));
+
+  // TODO: add slides data in DB
+  const slides = [
+    {
+      image: "https://sho-farm.sunnyday.jp/wp-content/uploads/2023/04/%E3%83%9F%E3%83%8B%E3%82%B5%E3%82%A4%E3%82%BA.jpg",
+      text: "Shoko and Sho welcome volunteers every Wednesday at their farm. As Adriana and I wanted to know more about permaculture, we decided to spend a day helping them. ",
+    },
+    {
+      image: "https://sho-farm.sunnyday.jp/wp-content/uploads/2023/04/%E3%83%9F%E3%83%8B%E3%82%B5%E3%82%A4%E3%82%BA.jpg",
+      text: "What stroke us first, was the deep connection between the farm and the surrounding nature. Bees and wasps roaming freely around the farm building feeding on the vines, spiders happily building their webs on chili pepper plants, all sorts of insects in the soil. It felt right to produce food in harmony with nature, in total opposition with the sterile fields of conventional agriculture. ",
+
+    },
+    {
+      image: "https://sho-farm.sunnyday.jp/wp-content/uploads/2023/04/%E3%83%9F%E3%83%8B%E3%82%B5%E3%82%A4%E3%82%BA.jpg",
+      text: "Shoko and Sho welcomed us, not as a workforce but rather as hosts, they took time to answer our questions, taught us how to farm, laughed with us, they invited us for lunch with produces of the farm and thank us with vegetable present when we left. We had a nice human experience on that day. ",
+    },
+    {
+      image: "https://sho-farm.sunnyday.jp/wp-content/uploads/2023/04/%E3%83%9F%E3%83%8B%E3%82%B5%E3%82%A4%E3%82%BA.jpg",
+      text: "A last thing we notice on that day is that their farm is professionally managed to make them and their coworker a living and to feed the local Yokosuka population. Very far from the, sometimes “hippie organic” you might see, their agriculture feels like a model that could fit a future society that can rely on fossil fuels and chemicals anymore.",
+    }
+  ]
   return (
     <div className="flex p-8 gap-8">
-      <div className="flex-grow">
+      <div className="flex-grow min-w-0">
         <div className="flex gap-8">
           <img
             className="w-1/3 h-[260px] object-cover object-[50%_20%] rounded-md"
@@ -34,10 +66,12 @@ const Project = async ({ params }: { params: { id: string } }) => {
           </div>
         </div>
         <ProjectDetailsMenu />
-        <div className="pb-8">
-          <h2 className="text-3xl">Who they are</h2>
-          <div className="flex">
-            <p className="w-1/2">
+        <div className="pb-16">
+          <h2 className="text-3xl mb-4" id="who-they-are">
+            Who they are
+          </h2>
+          <div className="flex gap-16">
+            <p className="w-1/2 text-justify">
               SHO Farm is located in Yokosuka City an hour and an half by train
               from Tokyo. They grow delicious organic vegetables mostly from
               native seeds without any chemical (pesticide, herbicide or
@@ -49,32 +83,129 @@ const Project = async ({ params }: { params: { id: string } }) => {
               local community and organize enjoyable events and different type
               of business collaboration projects.
             </p>
-            <p>SOME MAP</p>
+            <iframe
+              className="w-1/2 shadow-md rounded-md"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d93041.78173093272!2d139.6155857439024!3d35.21021137014839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60183e6a724ba9d5%3A0xfffc0592cff45bae!2sSHO%20Farm!5e0!3m2!1sen!2sjp!4v1727414178302!5m2!1sen!2sjp"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
           </div>
         </div>
-        <div className="pb-8">
-          <h2 className="text-3xl">How we met them</h2>
-          <div className="flex w-1/2">
-            <p>
-              SOME CAROUSEL WITH: Shoko and Sho welcome volunteers every
-              Wednesday at their farm. As Adriana and I wanted to know more
-              about permaculture, we decided to spend a day helping them.{' '}
-            </p>
-          </div>
+        <div className="pb-16">
+          <h2 className="text-3xl mb-4" id="how-we-met-them">
+            How we met them
+          </h2>
+            <ProjectCarousel slides={slides} />
         </div>
-        <div className="pb-8">
-          <h2 className="text-3xl">Our Mission</h2>
+        <div className="pb-16">
+          <h2 className="text-3xl mb-4" id="our-mission">
+            Our Mission
+          </h2>
           <Markdown className="mb-4">
             Shoko and Sho’s farm website has been around for quite sometimes and
             became cluttered and hard to maintain. They need a **website
             update** that will help them better sell their produce.
           </Markdown>
-          <div>
-            <ProjectMilestone date="September 4th 2024" description="We discovered Shoko and Sho outstanding farm and business and heard of their wish to improve their website " title="First meet" done={true} />
-            <ProjectMilestone date="September 27th 2024" description="We discovered Shoko and Sho outstanding farm and business and heard of their wish to improve their website " title="First meet" done={true} />
-            <ProjectMilestone date="October 1st 2024" description="We discovered Shoko and Sho outstanding farm and business and heard of their wish to improve their website " title="First meet" done={true} />
-            <ProjectMilestone date="TBD" description="We discovered Shoko and Sho outstanding farm and business and heard of their wish to improve their website " title="First meet" done={false} />
-            <ProjectMilestone date="TBD" description="We discovered Shoko and Sho outstanding farm and business and heard of their wish to improve their website " title="First meet" done={false} />
+          <div className='relative before:contents[""] before:absolute before:inset-0 before:bg-coralBlue before:w-1 before:h-full before:left-[30px] before:z-[-1]'>
+            <ProjectMilestone
+              date="September 4th 2024"
+              description="We discovered Shoko and Sho outstanding farm and business and heard of their wish to improve their website "
+              title="First meet"
+              done={true}
+            />
+            <ProjectMilestone
+              date="September 27th 2024"
+              description="We discovered Shoko and Sho outstanding farm and business and heard of their wish to improve their website "
+              title="First meet"
+              done={true}
+            />
+            <ProjectMilestone
+              date="October 1st 2024"
+              description="We discovered Shoko and Sho outstanding farm and business and heard of their wish to improve their website "
+              title="First meet"
+              done={true}
+            />
+            <ProjectMilestone
+              date="TBD"
+              description="We discovered Shoko and Sho outstanding farm and business and heard of their wish to improve their website "
+              title="First meet"
+              done={false}
+            />
+            <ProjectMilestone
+              date="TBD"
+              description="We discovered Shoko and Sho outstanding farm and business and heard of their wish to improve their website "
+              title="First meet"
+              done={false}
+            />
+          </div>
+        </div>
+        <div className="pb-16">
+          <h2 className="text-3xl mb-4" id="details-of-the-work">
+            Details of the Work
+          </h2>
+          <h3 className="text-2xl">Issues of the Current website</h3>
+          <p>
+            They currently have a blog type Wordpress website. The structure is
+            not well adapted to a business selling vegetable boxes. Also,
+            English and Japanese version of the text are displayed one below
+            each other on each page.
+          </p>
+          <iframe
+            style={{ zoom: 0.5 }}
+            className="w-[800px] h-[400px] object-cover my-3 shadow"
+            src="http://sho-farm.sunnyday.jp"
+            title="Original Website"
+          ></iframe>
+          <h3 className="text-2xl mt-6">Building a sustainable website</h3>
+          <Markdown>
+            Another important aspect is to make sure that Shoko and Sho could
+            maintain their website as much as possible. That it won’t impact
+            their business in terms of time and money.As much as possible we
+            will try to keep the maintenance fee (hosting fees mostly) at the
+            same level as before the update. We will teach them how to update
+            their website by themselves to have minimal external maintenance in
+            the future.
+          </Markdown>
+        </div>
+        <div className="pb-16">
+          <h2 className="text-3xl mb-4" id="links">
+            Links
+          </h2>
+          <div className="flex gap-8 ">
+            <Link target="_blank" href="http://sho-farm.sunnyday.jp">
+              <ImpactIcon
+                impact={{ text: 'Website', icon: faLaptop, color: 'black' }}
+              />
+            </Link>
+            <Link target="_blank" href="https://www.youtube.com/@shofarm2014">
+              <ImpactIcon
+                impact={{
+                  text: 'Youtube channel',
+                  icon: faSquareYoutube,
+                  color: 'black',
+                }}
+              />
+            </Link>
+            <Link target="_blank" href="https://www.facebook.com/2014sfarm">
+              <ImpactIcon
+                impact={{
+                  text: 'Facebook page',
+                  icon: faSquareFacebook,
+                  color: 'black',
+                }}
+              />
+            </Link>
+            <Link target="_blank" href="https://www.instagram.com/sho__farm/">
+              <ImpactIcon
+                impact={{
+                  text: 'Instagram channel',
+                  icon: faSquareInstagram,
+                  color: 'black',
+                }}
+              />
+            </Link>
           </div>
         </div>
       </div>
--- app/_components/ProjectDetailsMenu.tsx
+++ app/_components/ProjectDetailsMenu.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const ProjectDetailsMenu = () => {
   return (
-    <div className="my-8 flex gap-8 border-b-2 pb-2 font-heading text-2xl justify-around">
-      <Link href="#who-we-are">Who they are</Link>
-      <Link href="#how-we-meet-them">How we met them</Link>
+    <div className="my-16 flex gap-8 border-b-2 pb-2 font-heading text-2xl justify-around">
+      <Link href="#who-they-are">Who they are</Link>
+      <Link href="#how-we-met-them">How we met them</Link>
       <Link href="#our-mission">Our Mission</Link>
       <Link href="#details-of-the-work">Details of the Work</Link>
       <Link href="#links">Links</Link>
