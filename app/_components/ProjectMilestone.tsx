5223a29f51d5d6fefde024c3c4f7752c5d026331
Yann Klein <yann.klein@me.com>
2024-09-27 15:44:32 +0900

    modify project milestone style

--- app/_components/ProjectMilestone.tsx
+++ app/_components/ProjectMilestone.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const ProjectMilestone = ({date, description, title, done}) => {
+  const dynamicStyle = {
+    backgroundColor: done ? '#55A9BB' : "lightgrey",
+    borderColor: done ? "white" : "#55A9BB"
+  }
   return (
     <div className='flex items-center gap-4 mb-8'>
-      <div style={{backgroundColor: done ? '#55A9BB' : "lightgrey"}} className="w-16 h-16 flex justify-center items-center rounded-full text-white text-4xl">{done ? "✓" : ""}</div>
+      <div style={dynamicStyle} className="w-16 h-16 flex justify-center items-center rounded-full text-white text-4xl border-2">{done ? "✓" : ""}</div>
       <div>
         <h3 className='text-2xl'>{date}: {title}</h3>
         <p>{description}</p>
