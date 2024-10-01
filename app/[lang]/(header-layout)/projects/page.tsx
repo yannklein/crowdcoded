d4c33f030481bbffcc03b55459941c49d9415a6b
Yann Klein <yann.klein@me.com>
2024-10-01 13:57:47 +0900

    hydrate landing page

--- app/[lang]/(header-layout)/projects/page.tsx
+++ app/[lang]/(header-layout)/projects/page.tsx
@@ -7,10 +7,10 @@ import { getDictionary } from '../../dictionaries';
 
 const ProjectsIndex = async ({ params: { lang }}: { params: { lang: string } }) => {
   const dict = await getDictionary(lang);
-  const projects: ProjectProps[] = await getProjects();
+  const projects: ProjectProps[] = await getProjects(lang);
   return (
     <div className='min-h-[75vh] sm:min-h-auto'>
-      <h1 className=" text-5xl md:text-7xl text-center my-10" >{projects.length} on-going project{projects.length > 1 ? 's' : ''}</h1>
+      <h1 className=" text-5xl md:text-7xl text-center my-10" >{projects.length} {projects.length > 1 ? dict.projects.ongoings : dict.projects.ongoing}</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 m-2 sm:m-10">
         {projects.map((project: ProjectProps) => (
           <Project key={project.id} project={project} dict={dict}/>
