05f717f69dbc7d57d1050e4fea527aa35b0074af
Yann Klein <yann.klein@me.com>
2024-10-01 12:27:55 +0900

    add lang to projects page

--- app/[lang]/(header-layout)/projects/page.tsx
+++ app/[lang]/(header-layout)/projects/page.tsx
@@ -3,15 +3,17 @@ import React from 'react';
 import Project from '@/app/_components/Project';
 import { ProjectProps } from '@/app/types/Props';
 import { getProjects } from '@/utils/getProjects';
+import { getDictionary } from '../../dictionaries';
 
-const ProjectsIndex = async () => {
+const ProjectsIndex = async ({ params: { lang }}: { params: { lang: string } }) => {
+  const dict = await getDictionary(lang);
   const projects: ProjectProps[] = await getProjects();
   return (
     <div className='min-h-[75vh] sm:min-h-auto'>
       <h1 className=" text-5xl md:text-7xl text-center my-10" >{projects.length} on-going project{projects.length > 1 ? 's' : ''}</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 m-2 sm:m-10">
         {projects.map((project: ProjectProps) => (
-          <Project key={project.id} project={project} />
+          <Project key={project.id} project={project} dict={dict}/>
         ))}
       </div>
     </div>
