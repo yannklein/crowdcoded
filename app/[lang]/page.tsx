a29be0efb2f8d696c298c020881edab55d2b6774
Yann Klein <yann.klein@me.com>
2024-10-02 10:58:25 +0900

    fix spacing and font size on landing

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -19,7 +19,7 @@ const Landing = async ({ params: { lang } }) => {
           <LandingBanner dict={dict} />
           <div className="page py-20 px-3 md:px-8">
             <div className="grid md:grid-cols-3 gap-10">
-              <div className="font-heading flex justify-center text-center items-center text-5xl md:text-6xl">
+              <div className="font-heading flex justify-center text-center items-center text-3xl md:text-6xl">
                 {dict.landing.featured.title}
               </div>
               <div className="md:col-span-2">
--- app/_components/LandingBanner.tsx
+++ app/_components/LandingBanner.tsx
@@ -4,8 +4,8 @@ import LangSwitcher from './LangSwitcher';
 
 const LandingBanner = ({ dict }) => {
   return (
-    <div className="relative bg-lightBlue h-80 pb-4 lg:h-[28rem] flex flex-col items-center justify-center text-center drop-shadow-2xl ">
-      <div className="absolute top-4 right-9 flex space-x-6 items-center font-heading">
+    <div className="relative bg-lightBlue pb-24 pt-32 sm:pt-24 flex flex-col items-center justify-center text-center drop-shadow-2xl ">
+      <div className="absolute top-4 sm:right-9 flex space-x-6 items-center font-heading">
         <LangSwitcher />
         <Link href="projects" className="text-3xl">
           {dict.landing.banner.projects}
@@ -22,23 +22,27 @@ const LandingBanner = ({ dict }) => {
           width={100}
           height={100}
         />
-        <h1 className="text-5xl lg:text-9xl mt-4 font-heading">
+        <h1 className="text-6xl lg:text-9xl mt-4 font-heading">
           {dict.landing.banner.title}
         </h1>
       </div>
-      <p className="mt-4 text-lg md:text-xl px-2 lg:px-0">
-        {dict.landing.banner.subtitle1}
-      </p>
-      <p className="text-lg md:text-xl px-2 lg:px-0">
-        {dict.landing.banner.subtitle2}{' '}
-        <span className="text-coralBlue">{dict.landing.banner.subtitle3}</span>
-        {dict.landing.banner.subtitle4}
-      </p>
-      <p className="text-lg md:text-xl px-2 lg:px-0">
-        {dict.landing.banner.subtitle5}{' '}
-        <span className="text-coralBlue">{dict.landing.banner.subtitle6}</span>{' '}
-        {dict.landing.banner.subtitle7}
-      </p>
+      <div className="mt-4 text-md md:text-2xl px-4 lg:px-0 font-light">
+        <p className='mb-2 sm:mb-0'>{dict.landing.banner.subtitle1}</p>
+        <p>
+          {dict.landing.banner.subtitle2}{' '}
+          <span className="text-coralBlue">
+            {dict.landing.banner.subtitle3}
+          </span>
+          {dict.landing.banner.subtitle4}
+        </p>
+        <p className='mb-2 sm:mb-0'>
+          {dict.landing.banner.subtitle5}{' '}
+          <span className="text-coralBlue">
+            {dict.landing.banner.subtitle6}
+          </span>{' '}
+          {dict.landing.banner.subtitle7}
+        </p>
+      </div>
     </div>
   );
 };
